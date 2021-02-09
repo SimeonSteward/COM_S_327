1e9ba9c9dfe2d43fea28490165f0219a306ac0aa
SimeonSteward <simeontsteward@gmail.com>
2021-02-08 21:23:32 -0600

    Added Clion files and assignment 1

---
+++ Simeon_Steward.assignment_1.00/dungeonMaker.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define WIDTH 80
+#define HEIGHT 21
+
+bool initializeMap();
+bool createRoom();
+void printMap();
